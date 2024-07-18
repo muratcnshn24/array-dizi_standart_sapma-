fee9ec8a5c7d8255bbe1087157bd553f6bff8f85
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:10:55 +0300

    Proje dosyası ekle.

---
+++ array(dizi_standart_sapma)/Dizi.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace array_dizi_standart_sapma_
+{
+    class Dizi
+    {
+        ///<sammary>
+        /// tek boyutlu dizi oluşturmak için kullanılan fonksiyondur.
+        ///</sammary>
+        ///<param name="Limit"> dizinin uzunluğunu belirtir </param>
+        ///<returns> belirtilen uzunlukta rastgele sayıyılardan oluşmuş diziyi döner  </returns>
+        public int[] Olustur(int Limit)
+        {
+            int[] sayilar = new int[Limit];
+            Console.WriteLine("");
+            for (int i = 0; i < Limit; i++)
+            {
+                sayilar[i] = new Random().Next(1, 100);
+                Console.WriteLine("{0,3}", sayilar[i]);
+            }
+            Console.WriteLine(""); // dizi karakterleri yan yana yazılsın diye alt ve üste bu kısmı yazıp diziyi  araya aldım
+            return sayilar;
+
+        }
+
+        ///<sammary>
+        ///  Parametre olarak aldığı dizideki en büyük elemanı döner.
+        ///</sammary>
+        ///<param name="Dizi"> Dizi </param>
+        ///<returns></returns>
+        public static int EnBuyuk(int[] dizi)
+        {
+            int eb = dizi[0];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] > eb) eb = dizi[i];
+
+            }
+            return eb;
+        }
+
+        ///<sammary>
+        ///  Parametre olarak aldığı dizideki en küçük elemanı döner.
+        ///</sammary>
+        ///<param name="Dizi"> Dizi </param>
+        ///<returns></returns>
+        public static int EnKucuk(int[] dizi)                  //statik yaparsan nesne üretmene gerek kalmaz
+        {
+            int ek = dizi[0];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] < ek) ek = dizi[i];
+
+            }
+            return ek;
+        }
+        ///<sammary>
+        /// ParametrE olarak aldığı dizinin aritmetik ortalamasını hesaplar ve döner.
+        ///</sammary>
+        ///<param name="X">Dizi</param>
+        ///<returns>Aritmetik Ortalama</returns>
+
+        public static double AritmetikOrtalama(int[] X)
+        {
+            double toplam = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                toplam += X[i];
+            }
+            return toplam / X.Length;
+        }
+
+
+        ///<sammary>
+        ///  Parametre olarak aldığı dizinin standar sapmasini hesaplar.
+        ///</sammary>
+        ///<param name="X"> Dizi </param>
+        ///<returns>Standart Sapma</returns>
+
+        public static double StandartSapma(int[] X)
+        {
+            double aritmetikOrtalama = AritmetikOrtalama(X);
+            double t = 0, f = 0;
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                f = X[i] - aritmetikOrtalama;
+                t += Math.Pow(f, 2);             //Pow üst alma fonksiyonudur.
+
+             
+                        
+            }
+            return Math.Sqrt(t / (X.Length - 1)); // Sqrt karakök fonksiyonu.
+        }
+    }
+}
---
+++ array(dizi_standart_sapma)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace array_dizi_standart_sapma_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n degerini giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] X = new Dizi().Olustur(n);
+
+            Console.WriteLine("Dizinin Standart Sapmasi: {0:F2}", Dizi.StandartSapma(X));
+        }
+    }
+}
